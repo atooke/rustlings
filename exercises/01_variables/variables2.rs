0047013ba0a4eedfba573fb81b973e2ff05b6828
atooke <andy.tooke@mckesson.com>
2024-08-19 09:03:37 -0400

    init my rustlings repo

---
+++ exercises/01_variables/variables2.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+
+    // all on one line
+    //let x: i64 = 0;
+
+    //multiple line defintion
+    let x: i64;
+    x = 32;
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
---
+++ solutions/01_variables/variables5.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let number = "T-H-R-E-E";
+    println!("Spell a number: {}", number);
+
+    // Using variable shadowing
+    // https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html#shadowing
+    // add in let to change value
+    let number = 3;
+    println!("Number plus two is: {}", number + 2);
+}
